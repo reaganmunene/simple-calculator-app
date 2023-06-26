4adb5aee18eb339fa4826537e8672b4aa37fd27f
reaganmunene <reagannmunene@gmail.com>
2023-06-26 19:27:39 +0300

    completed assignment

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,18 @@
+# puts ("The multiplication is " + multiply("3", "2").to_s)
+puts "Welcome to a simple Calculator developed by Maureen Omamo."
+puts "This calculator will give allow you to enter two values."
+puts "It will calculate: Addition, Multiplication, Division, and Subtraction."
+puts "Please Enter the First Number:"
+num1 = gets.chomp()
+puts "Please Enter The Second Number:"
+num2 = gets.chomp()
+puts "Please the Mode of Operation [+ | - | / | *]:"
+operation = gets.chomp()
+
+
+puts ("You have entered the values: " + num1 + " and " + num2)
+
+
 def calculator(operation, num1, num2)
   # your code here
   answer = case operation
